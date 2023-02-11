8b8e78a5c2f9043e3320befde5d7373250dc25a6
Samuel Banya <sbanya@fastmail.com>
2023-02-11 17:23:18 -0600

    Taking out comments to see if that helps it actually work

--- jellyfin.sh
+++ jellyfin.sh
@@ -2,11 +2,11 @@
 docker run -d \
  --name jellyfin \
  --user 1000:1000 \
- --device /dev/dri/renderD128:/dev/dri/renderD128 \ # CUSTOM CONFIG: For Hardware Acceleration
- --device /dev/dri/card0:/dev/dri/card0 \ # CUSTOM CONFIG: For Hardware Acceleration
+ --device /dev/dri/renderD128:/dev/dri/renderD128 \
+ --device /dev/dri/card0:/dev/dri/card0 \
  --net=host \
- --volume /home/sam/jellyfin/config:/config \ # Alternatively --volume jellyfin-config:/config
- --volume /home/sam/jellyfin/cache:/cache \ # Alternatively --volume jellyfin-cache:/cache
+ --volume /home/sam/jellyfin/config:/config \
+ --volume /home/sam/jellyfin/cache:/cache \
  --mount type=bind,source=/media/REDHDD/Movies,target=/media \
  --restart=unless-stopped \
  jellyfin/jellyfin
